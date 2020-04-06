8a68ae207d8030a7b985ac5846c97570e666cb3b
ArunimJoarder <reevu123@gmail.com>
2020-04-07 00:32:32 +0530

    added check servers

--- src/automi_manager/src/behaviour_controller.py
+++ src/automi_manager/src/behaviour_controller.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+# Import libraries
 import rospy
 import atexit
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+
+# Import utility files
 from comm_setup import *
 import startup
 
@@ -13,9 +16,41 @@ import stand
 import rise
 import initialise
 
+# Import Messages and Services
+from automi_manager.srv import switchTrigger
+from std_srvs.srv import Trigger
+
 def on_shutdown():
     # close_ports()
-    print('\ngot into \'on_shutdown\'\n')
+    print('closing Dynamixel ports')
+
+def trySwitch():
+    rospy.wait_for_service('checkSwitch')
+    try:
+        checkSwitch = rospy.ServiceProxy("checkSwitch", switchTrigger)
+        response = checkSwitch(rospy.get_param("/switches/check"))
+
+        rospy.set_param("/switches/behave_mode", response.mode)
+        rospy.set_param("/switches/switch_index", response.mode_index)
+        print(response.mode, response.mode_index)
+    except rospy.ServiceException:
+        print("Service call for \'checkSwitch\' failed")
+
+def findStatus():
+    rospy.wait_for_service("checkDynamixelStatus")
+    try:
+        checkStatus = rospy.ServiceProxy("checkDynamixelStatus", Trigger)
+        response = checkStatus()
+        
+        if not response.success:            # If there is Hardware Error in Dynamixels
+            rospy.set_param("/switches/behave_mode", 'stand')
+            rospy.set_param("/switches/switch_index", 0)
+        
+        else:
+            pass
+
+    except rospy.ServiceException:
+        print("Service call for \'checkDynamixelStatus\' failed")
 
 def behaviourControl():
     # open_ports()
@@ -25,23 +60,43 @@ def behaviourControl():
     rate = rospy.Rate(10) # 10hz
     
     if rospy.get_param("/switches/startup"):
-        # startup.setup()
+        # startup.setup(biped_portHandler, torso_portHandler)
         rospy.set_param("/switches/startup", False)
-        print('/switches/startup = ', rospy.get_param("/switches/startup"))
+        print('\'/switches/startup\' = ', rospy.get_param("/switches/startup"))
     
     if not rospy.get_param("/switches/initialised"):
         # initialise.init(biped_portHandler, torso_portHandler)
         rospy.set_param("/switches/initialised", True)
-        print('/switches/initialised = ', rospy.get_param("/switches/initialised"))
+        print('\'/switches/initialised\' = ', rospy.get_param("/switches/initialised"))
     
     while not rospy.get_param("/switches/autonomous"):
         r = input('Press ENTER to continue')
-        print('passed input line')
+        # print('passed input line')
         rospy.set_param("/switches/autonomous", True)
-        print('/switches/autonomous = ', rospy.get_param("/switches/autonomous"))
+        print('\'/switches/autonomous\' = ', rospy.get_param("/switches/autonomous"))
 
-
+    print('check')
     while not rospy.is_shutdown():
+
+        trySwitch()                 # checks conditions to switch to particular behaviour
+
+        findStatus()                # checks whether any Dynamixel has had Hardware Error
+
+        switch = rospy.get_param("/switches/switch_index")
+        
+        if switch == 0:
+            # stand.stand(biped_portHandler, torso_portHandler)
+            pass
+        elif switch == 1:
+            # walk.walk(biped_portHandler, torso_portHandler)
+            pass
+        elif switch == 2:
+            # turn.turn(biped_portHandler, torso_portHandler)
+            pass
+        elif switch == 4:
+            # rise.rise(biped_portHandler, torso_portHandler)
+            pass
+        print('2')
         pass
         
 
@@ -50,5 +105,4 @@ if __name__ == '__main__':
         behaviourControl()
         atexit.register(on_shutdown)
     except rospy.ROSInterruptException:
-        # print('got here')
         pass
--- src/automi_manager/src/startup.py
+++ src/automi_manager/src/startup.py
@@ -2,12 +2,12 @@
 
 import rospy
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
-from comm_setup import biped_portHandler, torso_portHandler
 
-def setup():
+def setup(biped_portHandler, torso_portHandler):
     packetHandler = [PacketHandler(2.0), PacketHandler(1.0)]
 
     # Get setup parameters
+        # MX Parameters
     drive_mode  = rospy.get_param("/dynamixel_config/MX_2/drive_mode")
     prf_vel     = rospy.get_param("/dynamixel_config/MX_2/prf_vel")
     prf_acc     = rospy.get_param("/dynamixel_config/MX_2/prf_acc")
@@ -15,37 +15,49 @@ def setup():
     p_gain_pos  = rospy.get_param("/dynamixel_config/MX_2/p_gain_pos")
     i_gain_pos  = rospy.get_param("/dynamixel_config/MX_2/i_gain_pos")
     d_gain_pos  = rospy.get_param("/dynamixel_config/MX_2/d_gain_pos")
-
+        # AX Parameters
     moving_vel  = rospy.get_param("/dynamixel_config/AX_1/moving_vel")
+
+    # Get setup values
+        # MX Values
+    drive_mode_value  = 4
+    prf_vel_value     = rospy.get_param("/general/prf_vel")
+    prf_acc_value     = rospy.get_param("/general/prf_acc")
+    max_vel_value     = rospy.get_param("/general/max_vel")
+    p_gain_pos_value  = rospy.get_param("/general/p_gain_pos")
+    i_gain_pos_value  = rospy.get_param("/general/i_gain_pos")
+    d_gain_pos_value  = rospy.get_param("/general/d_gain_pos")
+        # AX Values
+    moving_vel_value  = rospy.get_param("/general/moving_vel")
 
     # Biped setup parameters
     biped_dxls  = rospy.get_param("/general/biped_dxls")
 
     for i in range(len(biped_dxls)):
-        if (biped_dxls[i][1] == 2):
-            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i][0], drive_mode[0], 4)
-            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i][0], prf_vel[0], rospy.get_param("/general/prf_vel"))
-            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i][0], prf_acc[0], rospy.get_param("/general/prf_acc"))
-            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i][0], max_vel[0], rospy.get_param("/general/max_vel"))
-            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i][0], p_gain_pos[0], rospy.get_param("/general/p_gain_pos"))
-            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i][0], i_gain_pos[0], rospy.get_param("/general/i_gain_pos"))
-            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i][0], d_gain_pos[0], rospy.get_param("/general/d_gain_pos"))
+        if (biped_dxls[i]['protocol'] == 2):
+            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i]['id'], drive_mode['addr'], drive_mode_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i]['id'], prf_vel['addr'], prf_vel_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i]['id'], prf_acc['addr'], prf_acc_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write4ByteTxRx(biped_portHandler, biped_dxls[i]['id'], max_vel['addr'], max_vel_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i]['id'], p_gain_pos['addr'], p_gain_pos_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i]['id'], i_gain_pos['addr'], i_gain_pos_value)
+            dxl_comm_result, dxl_error = packetHandler[0].write2ByteTxRx(biped_portHandler, biped_dxls[i]['id'], d_gain_pos['addr'], d_gain_pos_value)
         else:
-            dxl_comm_result, dxl_error = packetHandler[1].write2ByteTxRx(biped_portHandler, biped_dxls[i][0], moving_vel[0], rospy.get_param("/general/moving_vel"))
+            dxl_comm_result, dxl_error = packetHandler[1].write2ByteTxRx(biped_portHandler, biped_dxls[i]['id'], moving_vel['addr'], moving_vel_value)
         
         if dxl_comm_result != COMM_SUCCESS:
-            print(packetHandler[0].getTxRxResult(dxl_comm_result), biped_dxls[i][0])
+            print(packetHandler[0].getTxRxResult(dxl_comm_result), biped_dxls[i]['id'])
         elif dxl_error != 0:
-            print(packetHandler[0].getRxPacketError(dxl_error), biped_dxls[i][0])
+            print(packetHandler[0].getRxPacketError(dxl_error), biped_dxls[i]['id'])
 
     # Torso setup parameters
     torso_dxls = rospy.get_param("/general/torso_dxls")
 
     for i in range(len(torso_dxls)):
-        dxl_comm_result, dxl_error = packetHandler[1].write2ByteTxRx(torso_portHandler, torso_dxls[i][0], moving_vel[0], rospy.get_param("/general/moving_vel"))
+        dxl_comm_result, dxl_error = packetHandler[1].write2ByteTxRx(torso_portHandler, torso_dxls[i]['id'], moving_vel['addr'], moving_vel_value)
         
         if dxl_comm_result != COMM_SUCCESS:
-            print(packetHandler[1].getTxRxResult(dxl_comm_result), torso_dxls[i][0])
+            print(packetHandler[1].getTxRxResult(dxl_comm_result), torso_dxls[i]['id'])
         elif dxl_error != 0:
-            print(packetHandler[1].getRxPacketError(dxl_error), torso_dxls[i][0])
+            print(packetHandler[1].getRxPacketError(dxl_error), torso_dxls[i]['id'])
 
